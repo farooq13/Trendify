8417f3891c69b2bca0afa51cf8d251c1e1b22728
farooq13 <farukidris19@gmail.com>
2024-09-16 16:28:32 +0100

    working

--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+  
+class PostList(View):
+  def get(self, request, *args, **kwargs):
+
+    context = {}
+    return render(request, 'base/post_list.html', context)
