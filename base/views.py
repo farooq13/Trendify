b97ab674a8cf727a9a7d1591ac3ae5381fcea104
farooq13 <farukidris19@gmail.com>
2024-09-22 00:14:14 +0100

    UI/UX

--- base/views.py
+++ base/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Post, Comment, UserProfile
+from django.db.models import Q
+from .forms import PostForm
 
   
 class PostList(View):
   def get(self, request, *args, **kwargs):
+    posts = Post.objects.filter(
+      Q(author__profile__followers__in=[request.user.id]) |
+      Q(author=request.user)
+    )
+    form = PostForm()
+    
+    context = {
+      'posts': posts,
+      'form': form
+    }
+    return render(request, 'base/post_list.html', context)
+  
+  def post(self, request, *args, **kwargs):
+    posts = Post.objects.filter(
+      Q(author__profile__followers__in=[request.user.id]) |
+      Q(author=request.user)
+    )
+    form = PostForm(request.POST)
 
-    context = {}
+    context = {
+      'posts': posts,
+      'form': form
+    }
     return render(request, 'base/post_list.html', context)
